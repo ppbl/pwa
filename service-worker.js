5a738ffd3f388be9b1a1b72d7bb1df66757da0b6
DESKTOP-47G5AS5\mayn <820523733@qq.com>
2018-03-04 09:29:22 +0800

    index change

--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,10 @@ var cacheFiles = [
     "./index.html",
     "./111.jpg",
     "./2.jpg",
-    "./logo.png"
+    "./logo.png",
+    "./pwa.png"
 ];
-/* "/mdl/bower.json","/mdl/bower.json",
-    "/script/main.js", */
+/* 监听安装事件 caches.open 开启缓存 把缓存列表里的文件缓存到cacheStorage*/
 self.addEventListener("install", function(e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(caches.open(cacheName).then(function(cache) {
@@ -17,6 +17,7 @@ self.addEventListener("install", function(e) {
         return cache.addAll(cacheFiles);
     }));
 });
+/* 监听激活事件 对比cache 有更新的删除old*/
 self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
     e.waitUntil(
@@ -29,12 +30,10 @@ self.addEventListener('activate', function(e) {
         }));
       })
     );
-    return self.clients.claim();
+    return self.clients.claim(); // 可加可不加 似乎某些边界有用
   });
-/**
-为 fetch 事件添加一个事件监听器。接下来，使用 caches.match() 函数来检查传入的请求 URL 是否匹配当前缓存中存在的任何内容。如果存在的话，返回缓存的资源。
-如果资源并不存在于缓存当中，通过网络来获取资源，并将获取到的资源添加到缓存中。
-*/
+/** 为 fetch 事件添加一个事件监听器。接下来，使用 caches.match() 函数来检查传入的请求 URL 是否匹配当前缓存中存在的任何内容。如果存在的话，返回缓存的资源。
+如果资源并不存在于缓存当中，通过网络来获取资源，并将获取到的资源添加到缓存中。 */
 self.addEventListener("fetch", function(e) {
     e.respondWith(caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
--- task.js
+++ task.js
@@ -1,3 +1,4 @@
+    /* web worker */
     onmessage = function(e) {
     console.log('Message received from main script');
     var workerResult = 'Result: ' + (e.data[0] + e.data[1]);
